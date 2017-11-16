6c759345bc8c2e27c740b64d6a23b8df8ec9f54f
alice <rainingilove@gmail.com>
2017-11-16 14:07:31 +0100

    add ruby class

---
+++ ruby/hello.rb
@@ -0,0 +1,28 @@
+class Tree
+	attr_accessor :children, :node_name
+
+	def initialize(name, children=[])
+		@node_name = name
+		@children = children
+	end
+
+	def visit_all(&block)
+		visit &block
+		children.each {|c| c.visit_all &block}
+	end
+
+	def visit(&block)
+		block.call self
+	end
+end
+
+fish_tree = Tree.new('Seafish',
+	[Tree.new('dorade'), Tree.new('salmon')]
+)
+
+puts 'Visiting a node'
+fish_tree.visit {|node| puts node.node_name}
+puts
+
+puts 'Visiting entire tree'
+fish_tree.visit_all {|node| puts node.node_name}
