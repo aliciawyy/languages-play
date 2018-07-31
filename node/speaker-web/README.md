a12cf988109f0a1c229a396fcaa34e5ed15e5868
aliciawyy <rainingilove@gmail.com>
2018-07-31 17:00:49 +0200

    use speaker localy

---
+++ node/speaker-web/README.md
@@ -0,0 +1,13 @@
+Create a webapp project with
+
+```
+mkdir speaker-web
+cd speaker-web
+yo webapp
+```
+
+Start the application from command line
+
+```
+gulp serve
+```
--- node/speaker-web/app/scripts/main.js
+++ node/speaker-web/app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+console.log('Hello jQuery!');
+
+$(document).ready(function () {
+  function addSpeakersjQuery (speakers) {
+    $.each (speakers, function (index, speaker) {
+      let tbody = $('#speakers-tbody');
+      let tr = $('<tr></tr>');
+      let nameCol = $('<td></td>');
+      let aboutCol = $('<td></td>');
+
+      nameCol.text(speaker.firstName + ' ' + speaker.lastName);
+      aboutCol.text(speaker.about);
+
+      tr.append(nameCol);
+      tr.append(aboutCol);
+      tr.append(nameCol);
+      tbody.append(tr);
+    });
+  }
+
+  $.getJSON('json/speaker.json',
+    (data) => addSpeakersjQuery(data.speakers)
+  );
+});
