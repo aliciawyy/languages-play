c7b1bd26994ab9604def34602bc9009f52c24cc2
aliciawyy <rainingilove@gmail.com>
2018-07-31 10:51:33 +0200

    add speaker test

--- node/speaker/test/speakerTest.js
+++ node/speaker/test/speakerTest.js
@@ -14,6 +14,25 @@ describe('speakers', function () {
   it('should return a 200 response', function (done) {
     req.end(function (res) {
       expect(res.statusCode).to.eql(200);
-    })
+      expect(res.headers['content-type']).to.eql(
+        'application/json; charset=utf-8');
+      done();
+    });
   });
+
+  it('should return all speakers', function (done) {
+    /*
+    * req.end() executes the Unirest GET request asynchronously, the
+    * callback processed the HTTP response from the API call */
+    req.end(res => {
+      let speakers = res.body;
+      expect(speakers.length).to.eql(3);
+
+      let speaker3 = speakers[2];
+      expect(speaker3.company).to.eql('Talkola');
+      expect(speaker3.lastName).to.eql('Fisher');
+      expect(speaker3.tags).to.eql(['Java', 'Spring', 'Maven', 'REST']);
+      done();
+    });
+  })
 });
