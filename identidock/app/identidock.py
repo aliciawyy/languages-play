ea30b51889833d1e7f40ea16b2a68bfbf3c9731a
alice <rainingilove@gmail.com>
2018-08-06 15:50:45 +0200

    add the icon dnmonster

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "hello Docker!\n"
+def mainpage():
+    name = "Joe Bloggs"
+
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''
+      <form method="POST">
+      Hello <input type="text" name="name" value="{}">
+      <input type="submit" value="submit">
+      </form>
+      <p>You look like a:
+      <img src="/monster/monster.png"/>
+      '''.format(name)
+    footer = '</body></html>'
+    return header + body + footer
+
+
+@app.route("/monster/<name>")
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    return Response(r.content, mimetype="image/png")
 
 
 if __name__ == "__main__":
