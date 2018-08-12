fc3b6f31311be102a597aa607052966509551f0d
alice <rainingilove@gmail.com>
2018-08-12 10:43:41 +0200

    add erlang basic

---
+++ erlang/basic.erl
@@ -0,0 +1,29 @@
+% compile this module in erl shell with c(basic).
+-module(basic). % Defines the name of the module.
+-export([mirror/1]).
+-export([number/1]).
+% function to export, /1 means it has 1 parameter.
+-export([fib/1, factorial/1]).
+-export([count_words/1, count/1, report/1]).
+
+mirror(Anything) -> Anything.
+
+number(one) -> 1;
+number(two) -> 2;
+number(three) -> 3.
+
+fib(1) -> 1;
+fib(2) -> 1;
+fib(N) -> fib(N - 1) + fib(N - 2).
+
+factorial(1) -> 1;
+factorial(N) -> N * factorial(N - 1).
+
+count_words(String) -> string:words(String).
+
+count(1) -> io:fwrite(" 1~n", []);
+count(N) -> io:fwrite(" ~p~n", [N]), count(N - 1).
+
+
+report(success) -> io:fwrite("success");
+report({error, Message}) -> io:fwrite("error: message ~n", []).
