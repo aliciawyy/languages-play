75385de1ad3818479a448f9c7a3d4926cf21a222
Alice Wang <rainingilove@gmail.com>
2018-10-10 15:49:55 +0200

    use async for tests

--- elixir/blitzy/test/blitzy_test.exs
+++ elixir/blitzy/test/blitzy_test.exs
@@ -1,5 +1,10 @@
 defmodule BlitzyTest do
   use ExUnit.Case
   doctest Blitzy
-  
+
+  test "multiple async tasks" do
+    result = Blitzy.run(3, "errorurlnotexist")
+    assert length(result) == 3
+  end
+
 end
